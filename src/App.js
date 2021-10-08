78def8611cd99cfbf2349c55d22e899643696d76
Alex Hoskins <alexander.hoskins@yahoo.com>
2021-10-08 11:44:09 -0700

    Added fonts

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ const StyledContainer = styled.div`
   background-image: url(https://images.unsplash.com/photo-1588802822758-c3ccfa682d8d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80);
   color:#d2d7df;
   width:25%;
+  font-family: 'Shadows Into Light', cursive;
  
   }
 `
