50a979048592b6183739d68563ef5c10f99584e9
Alex Hoskins <alexander.hoskins@yahoo.com>
2021-10-08 11:05:02 -0700

    MVP almost done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character.js';
+import styled from 'styled-components';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +11,28 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [ starWarsData, setStarWarsData ] =useState([])
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+      .then(res => {
+        setStarWarsData(res.data);
+      }).catch(err => {
+        console.error(err);
+      })
+  }, [])
+
+  console.log(starWarsData)
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {/* {starWarsData && starWarsData.map(char)=> <Character char={char}/>} */}
+      {
+        starWarsData.map(char => {
+          return <Character char={char} key={char.height} />
+        })
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,16 @@
 // Write your Character component here
+import React, { useState, useEffect } from "react";
+import styled from 'styled-components';
+
+
+const Character = (props) => {  
+    
+    console.log(props.char)
+    
+    return (
+            <h2>{ props.char.name }</h2>
+        
+    )
+}
+
+export default Character;
